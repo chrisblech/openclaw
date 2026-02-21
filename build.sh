77ea2c3c3bebdba3d8bf9cb6bc6a67f6a9ea2496
chrisblech <chrisblech@users.noreply.github.com>
2026-02-21 11:55:43 +0000

    rename branch to "main-cb", aktualisiere deploy-skripte

--- build.sh
+++ build.sh
@@ -2,12 +2,14 @@
 
 IMAGE="private/openclaw-cb"
 DATE=$(date +%Y.%m.%d)
-TAG=${1:-$DATE}
+TIME=$(date +%H%M)
+TAG="${1:-$DATE}-$TIME"
 
 echo "Starte build $TAG"
 
 docker build \
-  --build-arg OPENCLAW_DOCKER_APT_PACKAGES="chromium jq python3-pip xauth xvfb" \
-  --shm-size=1g -t ${IMAGE}:${TAG} .
+  --build-arg OPENCLAW_DOCKER_APT_PACKAGES="jq nano python3-pip xauth" \
+  --build-arg OPENCLAW_INSTALL_BROWSER=1 \
+  --shm-size=1g -t ${IMAGE}:${TAG} -t ${IMAGE}:latest .
 
 echo "Build beendet - Tag: $TAG"
